f691c1d2764449e91fa4cca779c6ea67408769f9
brian <videticb@gmail.com>
2014-06-23 08:15:23 -0700

    second commit

---
+++ main.js
@@ -0,0 +1,399 @@
+var fa1={
+    name: 'Jonathan Towes',
+    team: 'Chicago Blackwawks',
+    number: 19,
+    position: 'center',
+    shoots: 'left',
+    gamesPlayed: 76, 
+    goals: 28,
+    assists: 40,
+    points: 68,
+    plusMinus: 26,
+    pim: 34,
+    ppg: 5,
+    shg:3,
+    gwg:5,
+    shots: 193,
+    sPercentage:14.5,
+    stanleyCupChampion: "yes",
+}
+
+var fa2={
+    name: 'Patrick Sharp',
+    team: 'Chicago Blackwawks',
+    number: 10,
+    position: 'left wing',
+    shoots: 'right',
+    gamesPlayed: 82, 
+    goals: 34,
+    assists: 44,
+    points: 78,
+    plusMinus: 13,
+    pim: 40,
+    ppg: 10,
+    shg:0,
+    gwg:3,
+    shots: 313,
+    sPercentage:10.9,
+    stanleyCupChampion: "yes",
+}
+
+var fa3={
+    name: 'Marian Hossa',
+    team: 'Chicago Blackwawks',
+    number: 81,
+    position: 'right wing',
+    shoots: 'left',
+    gamesPlayed: 72, 
+    goals: 30,
+    assists: 30,
+    points: 60,
+    plusMinus: 28,
+    pim: 20,
+    ppg: 4,
+    shg:3,
+    gwg:4,
+    shots: 241,
+    sPercentage:12.4,
+    stanleyCupChampion: "yes",
+}
+
+var fa4={
+    name: 'Andrew Shaw',
+    team: 'Chicago Blackwawks',
+    number: 65,
+    position: 'center',
+    shoots: 'right',
+    gamesPlayed: 80, 
+    goals: 20,
+    assists: 19,
+    points: 39,
+    plusMinus: 12,
+    pim: 76,
+    ppg: 5,
+    shg:0,
+    gwg:2,
+    shots: 149,
+    sPercentage:13.4,
+    stanleyCupChampion: "yes",
+}
+
+var fa5={
+    name: 'Patrick Kane',
+    team: 'Chicago Blackwawks',
+    number: 88,
+    position: 'right wing',
+    shoots: 'left',
+    gamesPlayed: 69, 
+    goals: 29,
+    assists: 40,
+    points: 69,
+    plusMinus: 7,
+    pim: 22,
+    ppg: 10,
+    shg:0,
+    gwg:6,
+    shots: 227,
+    sPercentage:12.8,
+    stanleyCupChampion: "yes",
+}
+
+var fa6={
+    name: 'Duncan Keith',
+    team: 'Chicago Blackwawks',
+    number: 2,
+    position: 'defense',
+    shoots: 'left',
+    gamesPlayed: 79, 
+    goals: 6,
+    assists: 55,
+    points: 61,
+    plusMinus: 22,
+    pim: 28,
+    ppg: 3,
+    shg:0,
+    gwg:3,
+    shots: 198,
+    sPercentage: 3.0,
+    stanleyCupChampion: "yes",
+}
+
+var fa7={
+    name: 'Corey Crawford',
+    team: 'Chicago Blackwawks',
+    number: 50,
+    gamesPlayed: 59,
+    gamesStarted: 56,
+    minPlayed: 3395,
+    goalsAgainstAvg: 2.26,
+    wins: 32,
+    losses: 16,
+    otLosses:10,
+    shutOuts: 2,
+    shotsAgainst:1533,
+    goalsAgains:128,
+    svPercentage: .917,
+    goals: 0,
+    assists: 1,
+    pim: 0,
+    stanleyCupChampion: "yes",
+}
+
+var fa8={
+    name: 'Brian Videtic',
+    team: 'Chicago Blackwawks',
+    number: 9,
+    position: 'right wing',
+    shoots: 'left',
+    gamesPlayed: 79, 
+    goals: 115,
+    assists: 75,
+    points: 190,
+    plusMinus: 1,
+    pim: 128,
+    ppg: 0,
+    shg:1,
+    gwg:9,
+    shots: 315,
+    sPercentage:11.1,
+    stanleyCupChampion: "no",
+
+}
+
+var fa9={
+    name: 'Troy Murray',
+    team: 'Chicago Blackwawks',
+    number: 29,
+    position: 'defense',
+    shoots: 'right',
+    gamesPlayed: 79, 
+    goals: 21,
+    assists: 25,
+    points: 46,
+    plusMinus: 2,
+    pim: 114,
+    ppg: 4,
+    shg:1,
+    gwg:7,
+    shots: 111,
+    sPercentage: 10.3,
+    stanleyCupChampion: "yes",
+}
+
+var fa10={
+    name: 'Steve Smith',
+    team: 'Chicago Blackwawks',
+    number: 5,
+    position: 'defense',
+    shoots: 'left',
+    gamesPlayed: 79, 
+    goals: 50,
+    assists: 25,
+    points: 75,
+    plusMinus: 1,
+    pim: 235,
+    ppg: 1,
+    shg:9,
+    gwg:3,
+    shots: 111,
+    sPercentage: 7.5,
+    stanleyCupChampion: "yes",
+}
+
+var fa11={
+    name: 'Wayne Gretzky',
+    team: 'Edmonton Oilers',
+    number: 99,
+    position: 'center',
+    shoots: 'left',
+    gamesPlayed: 79, 
+    goals: 100,
+    assists: 200,
+    points: 300,
+    plusMinus: 5,
+    pim: 70,
+    ppg: 9,
+    shg:3,
+    gwg:15,
+    shots: 401,
+    sPercentage: 15.3,
+    stanleyCupChampion: "yes",
+}
+
+var fa12={
+    name: 'Bob Probert',
+    team: 'Detroit Redwings',
+    number: 24,
+    position: 'defense',
+    shoots: 'right',
+    gamesPlayed: 74, 
+    goals: 29,
+    assists: 33,
+    points: 62,
+    plusMinus: 5,
+    pim: 398,
+    ppg: 16,
+    shg:16,
+    gwg:8,
+    shots: 201,
+    sPercentage: 10.3,
+    stanleyCupChampion: "yes",
+}
+
+var fa13={
+    name: 'Michael Nylander',
+    team: 'Chicago Blackwawks',
+    number: 92,
+    position: 'center',
+    shoots: 'left',
+    gamesPlayed: 82, 
+    goals: 25,
+    assists: 39,
+    points: 64,
+    plusMinus: 1,
+    pim: 36,
+    ppg: 11,
+    shg:0,
+    gwg:1,
+    shots: 176,
+    sPercentage: 10.3,
+    stanleyCupChampion: "yes",
+}
+
+var fa14={
+    name: 'Gary Suter',
+    team: 'Chicago Blackwawks',
+    number: 20,
+    position: 'defense',
+    shoots: 'right',
+    gamesPlayed: 81, 
+    goals: 30,
+    assists: 40,
+    points: 70,
+    plusMinus: 6,
+    pim: 110,
+    ppg: 0,
+    shg:11,
+    gwg:0,
+    shots: 111,
+    sPercentage: 9.3,
+    stanleyCupChampion: "yes",
+}
+
+var fa15={
+    name: 'Valerie Zelepukin',
+    team: 'Chicago Blackwawks',
+    number: 77,
+    position: 'right wing',
+    shoots: 'left',
+    gamesPlayed: 79, 
+    goals: 15,
+    assists: 40,
+    points: 55,
+    plusMinus: 9,
+    pim: 110,
+    ppg: 1,
+    shg:11,
+    gwg:1,
+    shots: 201,
+    sPercentage: 10.3,
+    stanleyCupChampion: "yes",
+}
+
+var fa16={
+    name: 'Chris Chelios',
+    team: 'Chicago Blackwawks',
+    number: 7,
+    position: 'defense',
+    shoots: 'left',
+    gamesPlayed: 79, 
+    goals: 20,
+    assists: 40,
+    points: 60,
+    plusMinus: 3,
+    pim: 310,
+    ppg: 9,
+    shg:0,
+    gwg:7,
+    shots: 201,
+    sPercentage: 10.3,
+    stanleyCupChampion: "yes",
+}
+
+var fa17={
+    name: 'Ed Belfour',
+    team: 'Chicago Blackwawks',
+    number: 00,
+    gamesPlayed: 61,
+    gamesStarted: 56,
+    minPlayed: 4000,
+    goalsAgainstAvg: 2.26,
+    wins: 42,
+    losses: 10,
+    otLosses:7,
+    shutOuts: 9,
+    shotsAgainst:2000,
+    goalsAgains:120,
+    svPercentage: .977,
+    goals: 1,
+    assists: 2,
+    pim: 2,
+    stanleyCupChampion: "yes",
+}
+
+var fa18={
+    name: 'Darren Pang',
+    team: 'Chicago Blackwawks',
+    number: 01,
+    gamesPlayed: 40,
+    gamesStarted: 28,
+    minPlayed: 4000,
+    goalsAgainstAvg: 3.26,
+    wins: 12,
+    losses: 26,
+    otLosses:19,
+    shutOuts: 1,
+    shotsAgainst:1211,
+    goalsAgains:228,
+    svPercentage: .117,
+    goals: 0,
+    assists: 0,
+    pim: 0,
+    stanleyCupChampion: "no",
+}
+
+var agents = [fa1, fa2, fa3, fa4, fa5, fa6, fa7, fa8, fa9, fa10, fa11, fa12, fa13, fa14, fa15, fa16, fa17, fa18];
+// var myArray = myString.split(' ');
+$(document).ready(function(){
+    $(function() {    
+        for (i in agents){
+            if (agents[i].number >= 0){
+               $(".agentList").append(agents[i].name);
+        };
+        };
+    });
+});
+
+// next step: seperate the array so the view is more appealing.
+// add a click event to the html page so agents can be added to the player list
+
+
+
+
+// THIS WAS BEING USED ON STATIC HTML PAGE
+// THIS WORKED TO MOVE THE PLAYER BUT CLONEED PREVIOUS CLICKS
+    // $("#freeAgents li").click(function(){
+    //     $(this).prependTo("#myTeam li");
+    // });
+
+
+// THIS WORKS TO MOVE THE PLAYER BUT CLONES PREVIOUS CLICKS
+    // $("#freeAgents li").click(function(){
+    //     $(this).appendTo("#myTeam li");
+    // });
+
+// THIS WORKS TO REMOVE THE PLAYERS THATS IT.
+//     $("#freeAgents li").click(function(){
+//         $(this).remove()
+//     });
+// });
